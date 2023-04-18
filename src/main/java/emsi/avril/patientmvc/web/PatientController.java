6e9254c93ca0a82f00c67522fc6dd8b1ff8094ae
KhaoulaElHattabi <khaoulaelhattabi00@gmail.com>
2023-04-18 23:51:01 +0000

    Creation d'une simple vue basée sur Thymeleaf

--- src/main/java/emsi/avril/patientmvc/web/PatientController.java
+++ src/main/java/emsi/avril/patientmvc/web/PatientController.java
@@ -1,14 +1,23 @@
 package emsi.avril.patientmvc.web;
 
+import emsi.avril.patientmvc.entities.Patient;
 import emsi.avril.patientmvc.repositories.PatientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
 public class PatientController {
     private PatientRepository patientRepository;
-
-
+    @GetMapping(path="/index")
+    public String patients(Model model){
+        List<Patient> patients = patientRepository.findAll();
+        model.addAttribute("ListPatients",patients);
+        return "patients";
+    }
 }
