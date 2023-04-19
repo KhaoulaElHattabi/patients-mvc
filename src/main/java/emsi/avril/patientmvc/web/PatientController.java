de7b1a1a072770376ea9f6edff2ee9d8ffbec42d
KhaoulaElHattabi <khaoulaelhattabi00@gmail.com>
2023-04-19 02:49:33 +0000

    Ajouter la suppression des patients

--- src/main/java/emsi/avril/patientmvc/web/PatientController.java
+++ src/main/java/emsi/avril/patientmvc/web/PatientController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -31,4 +32,20 @@ public class PatientController {
         model.addAttribute("keyword",keyword);
         return "patients";
     }
+    @GetMapping("/delete")
+    public String delete(Long id , String keyword , int page) {
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> lisPatients(){
+
+        return patientRepository.findAll();
+    }
+
 }
