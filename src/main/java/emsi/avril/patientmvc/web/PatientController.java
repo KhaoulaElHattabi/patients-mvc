9ddf22c738364658d934a0119c64f15fc8489f45
KhaoulaElHattabi <khaoulaelhattabi00@gmail.com>
2023-04-19 02:07:53 +0000

    Ajouter la pagination avec Thyymeleaf

--- src/main/java/emsi/avril/patientmvc/web/PatientController.java
+++ src/main/java/emsi/avril/patientmvc/web/PatientController.java
@@ -4,9 +4,12 @@ import emsi.avril.patientmvc.entities.Patient;
 import emsi.avril.patientmvc.repositories.PatientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,9 +18,15 @@ import java.util.List;
 public class PatientController {
     private PatientRepository patientRepository;
     @GetMapping(path="/index")
-    public String patients(Model model){
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("ListPatients",patients);
+    public String patients(Model model,
+                           @RequestParam(name="page", defaultValue = "0") int page,
+                           @RequestParam(name="size", defaultValue = "5") int size){
+        //List<Patient> patients = patientRepository.findAll();
+        //model.addAttribute("ListPatients",patients);
+        Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("ListPatients",pagePatients.getContent());
+        model.addAttribute("pages",new int [pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage",page);
         return "patients";
     }
 }
